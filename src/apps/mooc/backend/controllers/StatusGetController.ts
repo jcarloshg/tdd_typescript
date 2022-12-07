43b4a44b8617fadcfe5e84536fe5e4d29b6e37ee
jcarloshg <carlosj12336@gmail.com>
2022-12-06 18:26:38 -0600

    check healt

--- src/apps/mooc/backend/controllers/StatusGetController.ts
+++ src/apps/mooc/backend/controllers/StatusGetController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
-import httpStatus from 'http-status';
+// import httpStatus from 'http-status';
 import { Controller } from './Controller';
 
 export default class StatusGetController implements Controller {
   async run(req: Request, res: Response) {
-    res.status(httpStatus.OK).send();
+    // res.status(httpStatus.OK).send();
+    res.send(200).send();
   }
 }
